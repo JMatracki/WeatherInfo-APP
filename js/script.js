19689d7c637a21ea782bae16f3162a79bcf60fa9
JMatracki <jakub.matracki5@gmail.com>
2022-11-08 22:19:40 +0100

    release

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const downloadWeatherBtn = document.querySelector("#downloadWeather");
 const weatherInput = document.querySelector("#weatherInput");
 const weatherInfoDiv = document.querySelector("#weatherInfoContainer");
 const city = document.querySelector("#city");
-const temperature = document.querySelector("#temperature");
+const temperatureDivElement = document.querySelector("#temperature");
 const temperatureFilled = document.querySelector("#temperatureFilled");
 const windSpeed = document.querySelector("#windSpeed");
 const pressure = document.querySelector("#pressure");
@@ -10,18 +10,16 @@ const errorMessage = document.querySelector("#errorMessage");
 const icon = document.querySelector("#icon");
 const weatherDescription = document.querySelector("#weatherDescription");
 
-errorMessage.style.display = "none";
-
 const getWeatherApi = async () => {
   try {
     const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + weatherInput.value + '&appid=dba3cbbbe22de845daba54e9ad2c2c0b&units=metric&lang=PL');
     if (!response.ok) {
       throw 'Connection Failed';
     }
-    const data = await response.json();
-    return data;
+    return await response.json();
   }
   catch (error) {
+    console.error('Weather fetch error!');
   }
 }
 
@@ -30,20 +28,21 @@ const showWeather = async () => {
   if (weatherApi === undefined) {
     weatherInfoDiv.style.display = 'none';
     errorMessage.style.display = "block";
-    document.body.style.backgroundImage = "url('images/backgroundDefault.jpg')"
-    return errorMessage.innerText = "Blednie wpisana miejscowosc"
+    return document.body.style.backgroundImage = "url('images/backgroundDefault.gif')"
   }
 
   errorMessage.style.display = "none";
 
   const weatherMain = weatherApi.main;
-  const weatherWind = weatherApi.wind;
   const weatherIcon = weatherApi.weather;
 
+  const weatherKmSpeed = (weatherApi.wind.speed * 3.6).toFixed(2);
+  const weatherMsSpeed = weatherApi.wind.speed;
+
   city.innerText = weatherInput.value[0].toUpperCase() + weatherInput.value.slice(1);
-  temperature.innerText = 'Aktualna temperatura:' + ' ' + weatherMain.temp + '°C';
-  temperatureFilled.innerText = 'Aktualna odczuwalna temperatura:' + ' ' + weatherMain.feels_like + '°C';
-  windSpeed.innerText = 'Predkosc wiatru:' + ' ' + (weatherWind.speed * 3.6).toFixed(2) + 'km/h' + " | " + weatherWind.speed + 'm/s'
+  temperatureDivElement.innerText = 'Temperatura:' + ' ' + weatherMain.temp + '°C';
+  temperatureFilled.innerText = `Odczuwalna temperatura: ${weatherMain.feels_like} °C`;
+  windSpeed.innerText = `Predkosc wiatru: ${weatherKmSpeed} km/h | ${weatherMsSpeed} m/s`
   pressure.innerText = 'Cisnienie atmosferyczne:' + ' ' + weatherMain.pressure + 'hPa';
   weatherDescription.innerText = 'Opis pogody: ' + weatherIcon[0].description;
 
